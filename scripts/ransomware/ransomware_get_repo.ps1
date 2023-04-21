a257b4e594c3cb086bfd5b21371ee13ae334af8e
MohamedElatoubi <126865069+MohamedElatoubi@users.noreply.github.com>
2023-04-21 00:53:10 +0000

    Update ransomware_get_repo.ps1

--- scripts/ransomware/ransomware_get_repo.ps1
+++ scripts/ransomware/ransomware_get_repo.ps1
@@ -1,16 +1,10 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y git -Wait
-$Env:Path += ";C:\Program Files\Git\cmd"
-choco install -y go -Wait
-choco install -y make -Wait
+
 Set-MpPreference -DisableRealtimeMonitoring $true
 $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
 $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
 Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
 Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-$Env:Path += ';C:\ProgramData\chocolatey\bin\'
-$Env:GIT_REDIRECT_STDERR = '2>&1'
 cd\
 cd C:\Users\aiwadmin\Desktop
 git clone https://github.com/NextronSystems/ransomware-simulator.git
